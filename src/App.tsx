b5dc4257b2a82782799d6523156c52c4ccdab5d8
elcasey <temetvince@gmail.com>
2025-11-06 18:17:40 -0600

    Remove boids

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './website/Home/Home';
 
 import './App.css';
-import ECSDemo from './ecs/ECSDemo';
 
 export default function App() {
   return (
-    <div className='screen'>
-      <div id='ecs-demo'>
-        <ECSDemo transparency={0.2} />
+    <BrowserRouter>
+      <div className='app'>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+        </Routes>
       </div>
-      <BrowserRouter>
-        <div className='app'>
-          <Routes>
-            <Route
-              path='/'
-              element={<Home />}
-            />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   );
 }
